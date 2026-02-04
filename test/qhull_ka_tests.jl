b32bd54ecc56ab6ce097044ae449f76ab5915075
AlexisEnglebert <englebert.alexis@gmail.com>
2026-02-04 15:23:55 +0100

    Fixed tests & type error in MinMaxReduction

--- test/qhull_ka_tests.jl
+++ test/qhull_ka_tests.jl
@@ -1,22 +1,35 @@
 using Test
 using KernelAbstractions
 
-include(joinpath(@__DIR__, "..", "src", "qhull_kernel_abstraction.jl"))
-using .qhull_kernel_abstraction
+#include(joinpath(@__DIR__, "..", "src", "qhull_kernel_abstraction.jl"))
+include(joinpath(@__DIR__, "..", "src", "MinMaxReduction.jl"))
+include(joinpath(@__DIR__, "..", "src", "ScanPrimitive.jl"))
+#using .qhull_kernel_abstraction
+using .MinMaxReduction
+using .ScanPrimitive
 
 @testset "Segmented Scan" begin
-    @test segmented_scan([1, 2, 3, 4], [0, 0, 0, 0], (a, b) -> a + b) == [0, 1, 3, 6]
+    backend = CPU()
+    @test segmented_scan(backend, [1, 2, 3, 4], [0, 0, 0, 0], (a, b) -> a + b) == [0, 1, 3, 6]
 end
 
 @testset "Min Max reduce" begin
     backend = CPU()
-    min_max_values_ker = minmax_reduce(backend, 16)
     data = [2, 4, 6, 8, 10]
-    out = [(Inf64, Inf64)]
-    min_max_values_ker(data, out, ndrange=length(data))
-    @test out == [(2, 1)]
+    out = min_max_reduce(data, 16, backend)
+    
+    @test out.min == 2
+    @test out.imin == 1
+    @test out.max == 10
+    @test out.imax == 5
+
     data = [1, 2, 3, 4, 0]
-    min_max_values_ker(data, out, ndrange=length(data))
-    @test out  == [(0, 5)]
+    out = min_max_reduce(data, 16, backend)
+    
+    @test out.min == 0
+    @test out.imin == 5
+    @test out.max == 4
+    @test out.imax == 4
+
         
 end
