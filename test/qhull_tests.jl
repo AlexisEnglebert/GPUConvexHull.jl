7337c157e56d76038b50598aef3df45b88f8d7f0
AlexisEnglebert <englebert.alexis@gmail.com>
2026-04-20 14:59:52 +0200

    Added a new test that fails, code almost working

--- test/qhull_tests.jl
+++ test/qhull_tests.jl
@@ -20,9 +20,28 @@ end
     test_qhull(points, CPU(), expected)
 end
 
+@testset "Simple 3D shape" begin
+    points = [-4 -5 -3 -3 2 1 -5 -1 -1 -2
+               3  1  1  4 1 -5 -3 -3 1 -1
+              -3 -3 -3  1 1 -4 3  -1 1 0]
+
+    expected = [-4 -5 -3 2 1 -5  -1
+                 3  1  4 1 -5 -3 -3
+                -3 -3  1 1 -4 3  -1]
+
+#=[
+2.0 -3.0 1.0 -5.0 -4.0 -5.0 -3.0;
+1.0 4.0 -5.0 -3.0 3.0  1.0  1.0
+1.0 1.0 -4.0  3.0 -3.0 -3.0 -3.0
+] =#
+
+    test_qhull(points, CPU(), expected)
+end
+
+
 @testset "2D with colinear points " begin
     points = [ 0.0 -2.0  0.0  2.0  3.0 -1.0;
                2.0  0.0 -2.0  0.0  3.0  1.0 ]
-
     test_qhull(points, CPU(), points)
+
 end
