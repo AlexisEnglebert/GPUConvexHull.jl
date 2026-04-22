a48626ed7ddf1f0ebe7fd964d16e0f52989532c1
AlexisEnglebert <englebert.alexis@gmail.com>
2026-04-23 00:40:10 +0200

    fixed profile to v2

--- profile.jl
+++ profile.jl
@@ -1,8 +1,11 @@
 using Profile, ProfileView
+using GPUConvexHull
+using KernelAbstractions
 
 #Profile.init(delay=0.00001)
-data = rand(3, 100_000)
+data = rand(3, 10_000)
 GPUConvexHull.quick_hull(CPU(), data) 
 Profile.clear() 
 @profile for _ in 1:100; GPUConvexHull.quick_hull(CPU(), data); end
+Profile.print(format=:flat, sortedby=:count)
 ProfileView.view()
